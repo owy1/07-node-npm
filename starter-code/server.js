68d4f3d1a639002680cd118c307a17b580649f0d
Ophelia Yin <owyowy1@gmail.com>
2017-01-10 16:06:39 -0800

    build node.js server

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,20 +1,32 @@
 'use strict';
 
-// TODO: Initialize your project using NPM to create and populate a package.json file
+// DONE: Initialize your project using NPM to create and populate a package.json file
 
-// TODO: Require the Express package that you installed via NPM, and instantiate the app
+// DONE: Require the Express package that you installed via NPM, and instantiate the app
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 3000;
+// DONE: Include all of the static resources as an argument to app.use()
+app.use(express.static('./public'));
 
-// TODO: Include all of the static resources as an argument to app.use()
+app.get('./index.html', function(request, response) {
+  // DONE: Using the response object, send the index.html file back to the user
+  response.sendFile('index.html',{root: './public'});
+});
 
-app.get('*', function(request, response) {
-  // TODO: Using the response object, send the index.html file back to the user
+app.get('*', function(request,response){
+  console.log('new request:', request.url);
+  response.status(404).sendFile('404.html',{root:'./public'});
 });
 
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+app.get('/new.html', function(request,response){
+  response.sendFile('new.html',{root: './public'});
+});
 
 app.listen(PORT, function() {
-  // TODO: Log to the console a message that lets you know which port your server has started on
+  // DONE: Log to the console a message that lets you know which port your server has started on
+  console.log('server is up and running on port 5000 and can be accessd at th localhost:5000 in your browser');
 });
